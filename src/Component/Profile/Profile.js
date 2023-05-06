66f7b7c1bb5b4447877193152f37e4d2b82fa582
Ajay <ajey6162@gmail.com>
2023-05-06 10:47:25 +0530

    ...

--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 
 export function Profile() {
     const { user } = useSelector(state => state.user);
-    const { userPostList,isLoading } = useSelector(state => state.userPosts);
+    const { userPostList, isLoading } = useSelector(state => state.userPosts);
     const dispatch = useDispatch();
     useEffect(() => {
         (!userPostList.length) && dispatch(fetchPostById(user._id));
@@ -31,69 +31,69 @@ export function Profile() {
                                 <div className="col-sm-4"><span className="userSocials">1200 followers</span></div>
                                 <div className="col-sm-4"><span className="userSocials">108 following</span></div>
                             </div>
-                            {/* <div>
-                                    <button className="me-3" data-bs-toggle="modal" data-bs-target="#updateModal">Edit profile</button>
-                                    <img src="/img/gear-fill.svg"/>
-                                </div> */}
+                            <div>
+                                <button className="me-3" data-bs-toggle="modal" data-bs-target="#updateModal">Edit profile</button>
+                                <img src="/img/gear-fill.svg" />
+                            </div>
                         </ul>
                         <p className="bio">bio bio bio bio bio bio bio bio bio bio bio</p>
                     </div>
                 </div>
-                <hr/>
-                <section className="w-100" style={{display:"flex" , justifyContent:"center", alignItems:"center"}}>{(isLoading) && <Loader/>}</section>
+                <hr />
+                <section className="w-100" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>{(isLoading) && <Loader />}</section>
                 <div className="user-post-grid p-5">
-                    {userPostList.map((posts, index)=>
+                    {userPostList.map((posts, index) =>
                         <div key={index} className="product-item user-post bg-danger">
                             <img src={api.file + posts.file} />
                             <div className="product-action">
-                            <a className="btn" href="" style={{display:"flex",flexDirection:"column"}}><i className="bi bi-heart-fill" style={{color:"crimson",fontSize:"25px"}}></i><span style={{color:"white"}}> {posts.likeItems.length} likes</span></a>
-                           
+                                <a className="btn" href="" style={{ display: "flex", flexDirection: "column" }}><i className="bi bi-heart-fill" style={{ color: "crimson", fontSize: "25px" }}></i><span style={{ color: "white" }}> {posts.likeItems.length} likes</span></a>
+
+                            </div>
                         </div>
-                        </div>
-                        
+
                     )}
                 </div>
             </div>
         </div>
         <div className="modal fade" id="updateModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered">
-    <div className="modal-content">
-      <div className="modal-header" style={{backgroundColor:'#4abdac'}}>
-        <h5 className="modal-title" id="exampleModalLabel"style={{color:'white'}} >Updated Details</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-      <form >
-                <div className="row">
-                    <div className="col-md-4 mt-3 me-3">
-                        <img src="/img/user.png" style={{ height: '130px', width: '130px' }} />
-                        <button className=" btn btn-primary mt-3" style={{ width: "130px", backgroundColor: '#4abdac', border: 'none' }}>Edit Profile</button>
-
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header" style={{ backgroundColor: '#4abdac' }}>
+                        <h5 className="modal-title" id="exampleModalLabel" style={{ color: 'white' }} >Updated Details</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div className="col-md-7">
-                        <input type="text"  className="form-control mt-2" placeholder="Enter Name" />
-                        <input type="text"  className="form-control mt-2" placeholder="Enter UserName" />
-                        <input type="text"  className="form-control mt-2" placeholder="Enter Email" />
-                        <input type="text"  className="form-control mt-2" placeholder="Enter Contact" />
-                        <form className="mt-2 "  >
-                            <input type="radio" value='male' name="gender" /> male &nbsp;&nbsp;&nbsp;
-                            <input type="radio" value='female' name="gender" /> female
+                    <div className="modal-body">
+                        <form >
+                            <div className="row">
+                                <div className="col-md-4 mt-3 me-3">
+                                    <img src="/img/user.png" style={{ height: '130px', width: '130px' }} />
+                                    <button className=" btn btn-primary mt-3" style={{ width: "130px", backgroundColor: '#4abdac', border: 'none' }}>Edit Profile</button>
+
+                                </div>
+                                <div className="col-md-7">
+                                    <input type="text" className="form-control mt-2" placeholder="Enter Name" />
+                                    <input type="text" className="form-control mt-2" placeholder="Enter UserName" />
+                                    <input type="text" className="form-control mt-2" placeholder="Enter Email" />
+                                    <input type="text" className="form-control mt-2" placeholder="Enter Contact" />
+                                    <form className="mt-2 "  >
+                                        <input type="radio" value='male' name="gender" /> male &nbsp;&nbsp;&nbsp;
+                                        <input type="radio" value='female' name="gender" /> female
+                                    </form>
+
+                                </div>
+                            </div>
+                            <div className="row mt-2">
+                                <textarea placeholder="Add Address" className="form-control text1" style={{ width: '440px' }}  ></textarea>
+                            </div>
                         </form>
-
+                    </div>
+                    <div className="modal-footer">
+                        <Link to='/update' data-bs-dismiss="modal" style={{ marginRight: '240px' }} ><button className="btn btn-link" style={{ color: '#4abdac' }}>Become a artist</button> </Link>
+                        {/* <button  >Become a artist</button> */}
+                        <button type="button" className="btn btn-primary" style={{ backgroundColor: '#4abdac', border: 'none' }}>Update</button>
                     </div>
                 </div>
-                <div className="row mt-2">
-                    <textarea placeholder="Add Address" className="form-control text1" style={{width:'440px'}}  ></textarea>
-                </div>
-            </form>
-      </div>
-      <div className="modal-footer">
-        <Link to='/update' data-bs-dismiss="modal"style={{marginRight:'240px'}} ><button className="btn btn-link" style={{color:'#4abdac'}}>Become a artist</button> </Link>
-        {/* <button  >Become a artist</button> */}
-        <button type="button" className="btn btn-primary" style={{backgroundColor:'#4abdac',border:'none'}}>Update</button>
-      </div>
-    </div>
-  </div>
-</div>
+            </div>
+        </div>
     </>
 }
